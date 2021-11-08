4f34a967688672a393a2f3a429e6e23fc204fe0a
Carlos Pico <cpico6375@gmail.com>
2021-11-08 14:04:29 -0500

    restruccturación

--- pages/ranking/index.js
+++
@@ -1,6 +0,0 @@
-const ranking = ()=>{
-    return (
-        <div className="bg-green-400">ranquig</div>
-    );
-}
-export default ranking;
---
+++ src/pages/ranking/index.js
@@ -0,0 +1,11 @@
+import Header from "components/Header";
+
+const ranking = () => {
+    return (
+        <>
+            <Header />
+            <div className="bg-green-400">ranquig</div>
+        </>
+    );
+}
+export default ranking;
